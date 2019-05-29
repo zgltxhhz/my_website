c225642263755d77f6cdde8d2f952c6bbadee7c8
gaozhan <1822257749@qq.com>
2019-05-29 13:56:48 +0800

    重写导航

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-  	<nav class="navbar navbar-default navbar-fixed-top" :style="nav_style">
+  	<!-- <nav class="navbar navbar-default navbar-fixed-top" :style="nav_style">
 			<div class="container-fluid">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" 
@@ -14,14 +14,24 @@
 				    </button>
 					<a class="navbar-brand" href="#">logo</a>
 				</div>
-				<div :class="showClass" id="bs-example-navbar-collapse-1">
+				<div class="show" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
-						<li @click="hide_nav"><router-link to="/index">首页</router-link></li>
+						<li><router-link to="/index">首页</router-link></li>
 						<li @click="hide_nav"><router-link to="/study" @click="hide_nav">学习</router-link></li>
 						<li @click="hide_nav"><router-link to="/watching" @click="hide_nav">追番</router-link></li>
-						<li @click="hide_nav">login</li>
-						<!--<li><router-link to="/index">login</router-link></li>-->
+						<li>login</li>
 					</ul>
+				</div>
+			</div>
+		</nav> -->
+		<nav class="head-nav" :style="nav_style">
+			<div class="gao-logo"></div>
+			<div class="gao-link">
+				<div class="gao-btn">
+					111
+				</div>
+				<div class="gao-nav">
+					222
 				</div>
 			</div>
 		</nav>
@@ -37,8 +47,7 @@ export default {
 		name: 'app',
 		data() {
 			return {
-				nav_style: 'top: -50px',
-				showClass: 'collapse navbar-collapse'
+				nav_style: 'top: -5rem',
 			}
 		},
 		created(){},
@@ -48,13 +57,7 @@ export default {
 		methods:{
 			handleScroll(){
 				let scroll = document.documentElement.scrollTop
-				scroll >100? this.nav_style = 'top: 0' : this.nav_style = 'top: -50px;box-shadow: 0 0 0 #fff;'
-			},
-			hide_nav(){
-				document.documentElement.scrollTop = 0
-				// this.nav_style = 'top: -50px;box-shadow: 0 0 0 #fff;'
-				// console.log(111)
-				this.showClass = 'collapse navbar-collapse'
+				scroll >100? this.nav_style = 'top: 0' : this.nav_style = 'top: -5rem;box-shadow: 0 0 0 #fff;'
 			}
 		}
 	}
@@ -65,16 +68,56 @@ export default {
 		background: #f4f4f4;
 		padding-bottom: 1rem;
 		min-height: calc(100vh - 1rem);
-		.navbar{
+		// .navbar{
+		// 		transition: all .5s;
+		// 		box-shadow: 0px 0px 10px #c1c1c1;
+		// 		li{
+		// 			width: 60px;
+		// 			min-height: 50px;
+		// 			display: flex;
+		// 			flex-direction: row;
+		// 			align-items: center;
+		// 			justify-content: center;
+		// 		}
+		// 	}
+			.head-nav{
+				width: 100%;
+				height: 5rem;
+				background: #e8e8e8;
+				position: fixed;
+				top: 0;
 				transition: all .5s;
 				box-shadow: 0px 0px 10px #c1c1c1;
-				li{
-					width: 60px;
-					min-height: 50px;
+				z-index: 999;
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				.gao-logo{
+					width: 40%;
+					height: 100%;
+				}
+				.gao-link{
+					width: 60%;
+					height: 100%;
 					display: flex;
 					flex-direction: row;
 					align-items: center;
-					justify-content: center;
+					@media only screen and (max-width: 782px) {
+						.gao-btn{
+							display: block;
+						}
+						.gao-nav{
+							display: none;
+						}
+					}
+					@media only screen and (min-width: 782px) {
+						.gao-btn{
+							display: none;
+						}
+						.gao-nav{
+							display: block;
+						}
+					}
 				}
 			}
 			footer{
